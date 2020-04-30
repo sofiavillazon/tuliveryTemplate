955a5e0814858fa668805f9cc24c487bcd0694fa
sofia.villazon <sofiavillazon@gmail.com>
2020-04-30 00:04:07 -0500

    home

--- js/main.js
+++ js/main.js
@@ -1,3 +1,76 @@
+
+
+// SLIDER
+var auto = true;
+var pause = 7000;
+var $this = $('#slider');
+ 
+var slidesCont = $this.children('.slides-container');
+var slides = slidesCont.children('.slide');
+var arrowsCont = $this.children('.arrows');
+var prevSlide = arrowsCont.children('.prev');
+var nextSlide = arrowsCont.children('.next');
+var slidesCount = slides.length;
+var currentSlide = slides.first();
+var currentSlideIndex = 1;
+var autoPlay = null;
+
+slides.not(':first').css('display', 'none');
+currentSlide.addClass('active');
+
+function fadeNext() {
+    currentSlide.removeClass('active').fadeOut();
+ 
+    if(currentSlideIndex == slidesCount) {
+        currentSlide = slides.first();
+        currentSlide.delay().addClass('active').fadeIn();
+        currentSlideIndex = 1;
+    } else {
+        currentSlideIndex++;
+        currentSlide = currentSlide.next();
+        currentSlide.delay().addClass('active').fadeIn();
+    }
+}
+ 
+// Function responsible for fading to previous slide
+function fadePrev() {
+    currentSlide.removeClass('active').fadeOut();
+ 
+    if(currentSlideIndex == 1) {
+        currentSlide = slides.last();
+        currentSlide.delay().addClass('active').fadeIn();
+        currentSlideIndex = slidesCount;
+    } else {
+        currentSlideIndex--;
+        currentSlide = currentSlide.prev();
+        currentSlide.delay().addClass('active').fadeIn();
+    }
+}
+function AutoPlay() {
+    clearInterval(autoPlay);
+ 
+    if(auto == true)
+        autoPlay = setInterval(function() {fadeNext()}, pause);
+}
+
+$(nextSlide).click(function(e) {
+    e.preventDefault();
+    fadeNext();
+    AutoPlay();
+});
+ 
+$(prevSlide).click(function(e) {
+    e.preventDefault();
+    fadePrev();
+    AutoPlay();
+});
+ 
+AutoPlay();
+$(function(){
+    $(".imgvisor").popImg();
+})
+
+
 // duration of scroll animation
 var scrollDuration = 300;
 var leftPaddle = document.getElementsByClassName('left-paddle');
@@ -248,76 +321,4 @@ function myFunction() {
         }
     }
 }
-
-
-
-// SLIDER
-var auto = true;
-var pause = 7000;
-var $this = $('#slider');
- 
-var slidesCont = $this.children('.slides-container');
-var slides = slidesCont.children('.slide');
-var arrowsCont = $this.children('.arrows');
-var prevSlide = arrowsCont.children('.prev');
-var nextSlide = arrowsCont.children('.next');
-var slidesCount = slides.length;
-var currentSlide = slides.first();
-var currentSlideIndex = 1;
-var autoPlay = null;
-
-slides.not(':first').css('display', 'none');
-currentSlide.addClass('active');
-
-function fadeNext() {
-    currentSlide.removeClass('active').fadeOut();
- 
-    if(currentSlideIndex == slidesCount) {
-        currentSlide = slides.first();
-        currentSlide.delay().addClass('active').fadeIn();
-        currentSlideIndex = 1;
-    } else {
-        currentSlideIndex++;
-        currentSlide = currentSlide.next();
-        currentSlide.delay().addClass('active').fadeIn();
-    }
-}
- 
-// Function responsible for fading to previous slide
-function fadePrev() {
-    currentSlide.removeClass('active').fadeOut();
- 
-    if(currentSlideIndex == 1) {
-        currentSlide = slides.last();
-        currentSlide.delay().addClass('active').fadeIn();
-        currentSlideIndex = slidesCount;
-    } else {
-        currentSlideIndex--;
-        currentSlide = currentSlide.prev();
-        currentSlide.delay().addClass('active').fadeIn();
-    }
-}
-function AutoPlay() {
-    clearInterval(autoPlay);
- 
-    if(auto == true)
-        autoPlay = setInterval(function() {fadeNext()}, pause);
-}
-
-$(nextSlide).click(function(e) {
-    e.preventDefault();
-    fadeNext();
-    AutoPlay();
-});
- 
-$(prevSlide).click(function(e) {
-    e.preventDefault();
-    fadePrev();
-    AutoPlay();
-});
- 
-AutoPlay();
-$(function(){
-    $(".imgvisor").popImg();
-})
 
